07df4b94c4c7f5d09d82e140d8294ccefd1a003a
owj1209 <owj1209@naver.com>
2025-06-20 14:40:27 +0900

    10

--- src/main/java/com/example/cicdtest/HelloController.java
+++ src/main/java/com/example/cicdtest/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "#9 hello";
+		return "#10 hello";
 	}
 }
